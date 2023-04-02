1d78e1f983c4d9ed0cd1a44fea362d1b0c98b4a1
Nguyễn Thành Nhân <96537475+NBK111@users.noreply.github.com>
2023-04-03 01:35:32 +0700

    ChiTietHoaDon

---
+++ QuanLyThuoc_N3/src/quanlythuoc/ChiTietHoaDon.java
@@ -0,0 +1,67 @@
+package quanlythuoc;
+
+public class ChiTietHoaDon {
+	private double donGia;
+	private int soLuong;
+	private String donViTinh;
+	private double phiVAT;
+
+	// constructor
+	public ChiTietHoaDon() {
+		super();
+	}
+
+	public ChiTietHoaDon(double donGia, int soLuong, String donViTinh, double phiVAT) {
+		this.donGia = donGia;
+		this.soLuong = soLuong;
+		this.donViTinh = donViTinh;
+		this.phiVAT = phiVAT;
+	}
+
+	// getter-setter
+	public double getDonGia() {
+		return donGia;
+	}
+
+	public void setDonGia(double donGia) {
+		this.donGia = donGia;
+	}
+
+	public int getSoLuong() {
+		return soLuong;
+	}
+
+	public void setSoLuong(int soLuong) {
+		this.soLuong = soLuong;
+	}
+
+	public String getDonViTinh() {
+		return donViTinh;
+	}
+
+	public void setDonViTinh(String donViTinh) {
+		this.donViTinh = donViTinh;
+	}
+
+	public double getPhiVAT() {
+		return phiVAT;
+	}
+
+	public void setPhiVAT(double phiVAT) {
+		this.phiVAT = phiVAT;
+	}
+
+	// tinh tien phai tra
+	public double soTienPhaiTra() {
+		double tienChuaVAT = soLuong * donGia;
+		double tienVAT = tienChuaVAT * phiVAT / 100.0;
+		return 	tienVAT;
+	}
+	//toString
+	public String toString()
+	{
+		return "ChiTietHoaDon [donGia=" + donGia + ", soLuong=" + soLuong + ", donViTinh=" + donViTinh + ", phiVAT=" + phiVAT 
+				+ ", soTienPhaiTra=" + soTienPhaiTra() + "]";
+	}
+
+}
