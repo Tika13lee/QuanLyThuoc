c6bace4218cc2b96de748061bf2c51053f18b3f7
LeNguyenDuyKhang <duykhang0175@gmail.com>
2023-04-20 21:56:42 +0700

    frmQLHoaDon

--- QuanLyThuoc_N3/src/gui/FrmQLHoaDon.java
+++ QuanLyThuoc_N3/src/gui/FrmQLHoaDon.java
@@ -8,6 +8,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ItemEvent;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -17,7 +19,9 @@ import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
+import javax.swing.JTabbedPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
@@ -29,26 +33,33 @@ import com.toedter.calendar.JDateChooser;
 
 public class FrmQLHoaDon extends JFrame implements ActionListener {
 	private static final long serialVersionUID = 1L;
-	private JLabel lblMaHD, lblNgayLap, lblSDT, lblHoKH, lblTenKH, lblGioiTinh, lblDiaChi, lblCMND, lblNgaySinh,
-			lblTenThuoc, lblTuoi, lblTimThuoc;
-	private JLabel lblMaNV, lblHoTenNV, lblCMNDNV, lblDiaChiNV; // nhan vien
-	private JTextField txtMaNV, txtHoTenNV, txtCMNDNV, txtDiaChiNV;// nhan vien
-	private JTextField txtMaHD, txtNgayLap, txtSDT, txtHoKH, txtTenKH, txtDiaChi, txtNgaySinh, txtCMND;// KH
+	private JLabel lblMaHD, lblNgayLap, lblSDT, lblHoKH, lblTenKH, lblGioiTinh, lblDiaChi, lblNgaySinh,lblMaKH;
+	private JLabel lblMaNV, lblHoTenNV, lblSDTNV, lblDiaChiNV; // nhan vien
+	private JTextField txtMaNV, txtHoTenNV, txtSDTNV, txtDiaChiNV;// nhan vien
+	private JTextField txtMaHD, txtSDT, txtHoKH, txtTenKH, txtDiaChi,txtMaKH;// KH
 	private JDateChooser dcrNgayLap;
 	private JDateChooser dcrNgaySinh;
-	private JComboBox cboGioiTinh;
-	private JComboBox cboTimThuoc;
+	private JRadioButton radNam;
 	private DefaultTableModel model;
 	private JTable table;
 	private JButton btnThem, btnXoa, btnLamMoi, btnThanhToan, btnTimThuoc;
+	
+	private JTabbedPane tabbedPane;
+	//quan ly hoa don
+	private JLabel lblTimHDTheoMa,lblTimHDTheoNV;
+	private JComboBox<String> cboTimHDTheoMa;
+	private JComboBox<String> cboTimHDTheoNV;
+	private JButton btnXoaHD,btnThoat;
+	private DefaultTableModel model1;
+	private JTable table1;
 
 	public FrmQLHoaDon() {
 		// TODO Auto-generated constructor stub
 		JPanel pBorder = new JPanel();
 		pBorder.setLayout(new BorderLayout());
-		setTitle("Quản lý hóa đơn");
+		setTitle("Hóa đơn");
 		setExtendedState(MAXIMIZED_BOTH);
-//		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setResizable(false);
 		JPanel pNorth = new JPanel();
@@ -57,84 +68,84 @@ public class FrmQLHoaDon extends JFrame implements ActionListener {
 		Font font = new Font("Arial", Font.BOLD, 25);
 		lblTitle.setFont(font);
 		pNorth.add(lblTitle);
-		pNorth.setBorder(new EmptyBorder(20, 0, 0, 0));
+		pNorth.setBorder(new EmptyBorder(30, 0, 0, 0));
 		pBorder.add(pNorth, BorderLayout.NORTH);
 
 		JPanel pCenter = new JPanel();
 		pCenter.setLayout(new BoxLayout(pCenter, BoxLayout.Y_AXIS));
 
 		Box b1 = Box.createHorizontalBox();
-		// b1.add(Box.createVerticalStrut(15));
+		b1.add(Box.createVerticalStrut(15));
 		b1.add(lblMaHD = new JLabel("Mã hóa đơn:"));
 		b1.add(txtMaHD = new JTextField(15));
-		txtMaHD.setColumns(1);
-		lblMaHD.setPreferredSize(new Dimension(150, 20));
 		b1.add(Box.createHorizontalStrut(50));
+		
 		b1.add(lblNgayLap = new JLabel("Ngày lập hóa đơn:"));
-		// b1.add(txtNgayLap = new JTextField(15));
 		b1.add(dcrNgayLap = new JDateChooser());
-		b1.add(Box.createHorizontalStrut(210));
-
-		lblNgayLap.setPreferredSize(new Dimension(150, 20));
+		dcrNgayLap.setLocale(new Locale("vi", "VN"));
+		dcrNgayLap.setDateFormatString("dd/MM/yyyy");
+		dcrNgayLap.setDate(new Date(System.currentTimeMillis()));
+		b1.add(Box.createHorizontalStrut(50));
 		pCenter.add(Box.createVerticalStrut(20));
+		b1.add(lblMaKH = new JLabel("Mã khách hàng:"));
+		b1.add(txtMaKH = new JTextField(15));
 		pCenter.add(b1);
 
 		Box b2 = Box.createHorizontalBox();
 		b2.add(lblHoKH = new JLabel("Họ KH:"));
 		b2.add(txtHoKH = new JTextField(15));
-		lblHoKH.setPreferredSize(lblMaHD.getPreferredSize());
-		b2.add(Box.createHorizontalStrut(50));
+		b2.add(Box.createHorizontalStrut(125));
 		b2.add(lblTenKH = new JLabel("Tên KH:"));
 		b2.add(txtTenKH = new JTextField(15));
-		lblTenKH.setPreferredSize(lblNgayLap.getPreferredSize());
-		b2.add(Box.createHorizontalStrut(20));
+		b2.add(Box.createHorizontalStrut(50));
 		b2.add(lblGioiTinh = new JLabel("Giới tính:"));
-		lblGioiTinh.setPreferredSize(new Dimension(100, 20));
-		cboGioiTinh = new JComboBox<String>(new String[] { "Nam", "Nữ" });
-		cboGioiTinh.setPreferredSize(new Dimension(150, 20));
-		b2.add(cboGioiTinh);
+		radNam = new JRadioButton("Nam",true);
+		b2.add(radNam);
+		//b2.add(Box.createHorizontalStrut(200));
+		b2.add(Box.createHorizontalStrut(290));
 		pCenter.add(Box.createVerticalStrut(20));
 		pCenter.add(b2);
 
 		Box b3 = Box.createHorizontalBox();
 		b3.add(lblSDT = new JLabel("Số điện thoại KH:"));
-		lblSDT.setPreferredSize(lblTenKH.getPreferredSize());
 		b3.add(txtSDT = new JTextField(15));
-		b3.add(Box.createHorizontalStrut(190));
-		b3.add(lblCMND = new JLabel("Số CMND:"));
-		lblCMND.setPreferredSize(lblTenKH.getPreferredSize());
-		b3.add(txtCMND = new JTextField(15));
 		b3.add(Box.createHorizontalStrut(50));
 		b3.add(lblNgaySinh = new JLabel("Ngày sinh KH:"));
-		lblNgaySinh.setPreferredSize(lblGioiTinh.getPreferredSize());
 		// b3.add(txtNgaySinh = new JTextField(15));
 		b3.add(dcrNgaySinh = new JDateChooser());
+		dcrNgaySinh.setLocale(new Locale("vi", "VN"));
+		dcrNgaySinh.setDateFormatString("dd/MM/yyyy");
 		b3.add(Box.createHorizontalStrut(50));
+		b3.add(btnTimThuoc = new JButton("Tìm thuốc"));
+		b3.add(Box.createHorizontalStrut(340));
 		pCenter.add(Box.createVerticalStrut(20));
 		pCenter.add(b3);
 
 		Box b4 = Box.createHorizontalBox();
 		b4.add(lblDiaChi = new JLabel("Địa chỉ:"));
-		lblDiaChi.setPreferredSize(lblHoKH.getPreferredSize());
 		b4.add(txtDiaChi = new JTextField(30));
-		b4.add(Box.createHorizontalStrut(20));
-		b4.add(lblTimThuoc = new JLabel("Tìm thuốc:"));
-		lblTimThuoc.setPreferredSize(lblNgayLap.getPreferredSize());
-		cboTimThuoc = new JComboBox();
-		cboTimThuoc.setPreferredSize(new Dimension(300, 20));
-		b4.add(cboTimThuoc);
-		b4.add(Box.createHorizontalStrut(20));
-		b4.add(btnTimThuoc = new JButton("Tìm thuốc"));
-		b4.add(Box.createHorizontalStrut(300));
 		pCenter.add(Box.createVerticalStrut(20));
 		pCenter.add(b4);
+		
+		lblMaHD.setPreferredSize(new Dimension(150, 20));
+		lblNgayLap.setPreferredSize(new Dimension(150, 20));
+		lblMaKH.setPreferredSize(new Dimension(150, 20));
+		
+		lblHoKH.setPreferredSize(lblMaHD.getPreferredSize());
+		lblTenKH.setPreferredSize(lblNgayLap.getPreferredSize());
+		lblGioiTinh.setPreferredSize(lblMaKH.getPreferredSize());
+		
+		lblSDT.setPreferredSize(lblMaHD.getPreferredSize());
+		lblNgaySinh.setPreferredSize(lblMaKH.getPreferredSize());
+		btnTimThuoc.setPreferredSize(lblMaKH.getPreferredSize());
+		
+		lblDiaChi.setPreferredSize(lblMaHD.getPreferredSize());
 
 //		Dimension dms_lbl = new Dimension(100, 20);
 //		Arrays.stream(new JLabel[] { lblMaHD,lblNgayLap,lblSDT,lblHoTenKH,lblGioiTinh,lblDiaChi,lblCMND,lblNgaySinh,lblTenThuoc,lblTuoi,lblTimThuoc })
 //		.forEach(item -> item.setPreferredSize(dms_lbl));
 
-		String[] tb = new String[] { "STT", "MaHoaDon", "MaThuoc", "LoaiThuoc", "TenThuoc", "DonGia", "SoLuong",
-				"ThanhTien", "NgayLapHD", "HoTenKH", "SDTKH", "GioiTinh", "Tuoi" };
+		String[] tb = new String[] { "STT", "Mã thuốc","Tên thuốc","Mô tả","Đơn vị tính","Đơn giá bán","Số lượng","%VAT","Thành tiền" };
 		model = new DefaultTableModel(tb, 0);
 		table = new JTable(model);
 		JScrollPane sp = new JScrollPane(table, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
@@ -168,8 +179,8 @@ public class FrmQLHoaDon extends JFrame implements ActionListener {
 
 		Box b7 = Box.createHorizontalBox();
 		b7.add(Box.createHorizontalStrut(15));
-		b7.add(lblCMNDNV = new JLabel("Số CMND:"));
-		b7.add(txtCMNDNV = new JTextField(15));
+		b7.add(lblSDTNV = new JLabel("Số Điện thoại:"));
+		b7.add(txtSDTNV = new JTextField(15));
 		b7.add(Box.createHorizontalStrut(15));
 		// pWest.add(b7);
 
@@ -181,7 +192,7 @@ public class FrmQLHoaDon extends JFrame implements ActionListener {
 		// pWest.add(b8);
 		lblMaNV.setPreferredSize(new Dimension(150, 20));
 		lblHoTenNV.setPreferredSize(lblMaNV.getPreferredSize());
-		lblCMNDNV.setPreferredSize(lblMaNV.getPreferredSize());
+		lblSDTNV.setPreferredSize(lblMaNV.getPreferredSize());
 		lblDiaChiNV.setPreferredSize(lblMaNV.getPreferredSize());
 
 		b.add(Box.createVerticalStrut(15));
@@ -245,12 +256,84 @@ public class FrmQLHoaDon extends JFrame implements ActionListener {
 		pBorder.setBorder(new EmptyBorder(10, 20, 10, 20));
 
 		this.add(pBorder);
-		cboTimThuoc.addActionListener(this);
 		btnTimThuoc.addActionListener(this);
 		btnThem.addActionListener(this);
 		btnXoa.addActionListener(this);
 		btnLamMoi.addActionListener(this);
 		btnThanhToan.addActionListener(this);
+		
+		
+	//frm quan ly hoa don
+		JPanel pBorder1 = new JPanel();
+		pBorder1.setLayout(new BorderLayout());
+		
+		JPanel pNorth1 = new JPanel();
+		JLabel lblTitle1 = new JLabel("QUẢN LÝ HÓA ĐƠN");
+		lblTitle1.setForeground(Color.BLUE);
+		Font font1 = new Font("Arial", Font.BOLD, 30);
+		lblTitle1.setFont(font1);
+		pNorth1.add(lblTitle1);
+		pNorth1.setBorder(new EmptyBorder(20, 0, 0, 0));
+		pBorder1.add(pNorth1, BorderLayout.NORTH);
+		
+		JPanel pCenter1 = new JPanel();
+		pCenter1.setLayout(new BoxLayout(pCenter1, BoxLayout.Y_AXIS));
+		
+		Box b11 = Box.createHorizontalBox();
+		b11.add(lblTimHDTheoMa= new JLabel("Tìm theo mã hóa đơn:"));
+		b11.add(cboTimHDTheoMa = new JComboBox<String>());
+		cboTimHDTheoMa.setPreferredSize(new Dimension(200, 20));
+		b11.add(Box.createHorizontalStrut(500));
+		b11.add(btnXoaHD = new JButton("Xóa hóa đơn"));
+		pCenter1.add(Box.createVerticalStrut(50));
+		pCenter1.add(b11);
+		
+		Box b21 = Box.createHorizontalBox();
+		b21.add(lblTimHDTheoNV= new JLabel("Tìm theo mã nhân viên:"));
+		b21.add(cboTimHDTheoNV = new JComboBox<String>());
+		cboTimHDTheoNV.setPreferredSize(new Dimension(200, 20));
+		b21.add(Box.createHorizontalStrut(500));
+		b21.add(btnThoat = new JButton("Thoát"));
+		pCenter1.add(Box.createVerticalStrut(20));
+		pCenter1.add(b21);
+		pCenter1.add(Box.createVerticalStrut(20));
+		
+		lblTimHDTheoMa.setPreferredSize(new Dimension(200,20));
+		lblTimHDTheoNV.setPreferredSize(lblTimHDTheoMa.getPreferredSize());
+		
+		//table
+		String[] tb1 = new String[] { "STT", "Mã hóa đơn","Ngày lập hóa đơn","Mã nhân viên","Mã khách hàng"};
+		model1 = new DefaultTableModel(tb1, 0);
+		table1 = new JTable(model1);
+		JScrollPane sp1 = new JScrollPane(table1, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,
+				JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+		sp1.setPreferredSize(new Dimension(0, 600));
+		pCenter1.add(Box.createVerticalStrut(20));
+		pCenter1.add(sp1);
+		
+		pBorder1.add(pCenter1,BorderLayout.CENTER);
+		pBorder1.setBorder(new EmptyBorder(20, 50, 20, 50));
+		
+		
+		this.add(pBorder1);
+		cboTimHDTheoMa.addActionListener(this);
+		cboTimHDTheoNV.addActionListener(this);
+		btnThoat.addActionListener(this);
+		btnXoaHD.addActionListener(this);
+		
+		//tao tab
+				tabbedPane = new JTabbedPane();
+				tabbedPane.setForeground(new Color(0, 128, 128));
+				tabbedPane.setBounds(0, 0, 1535, 840);
+				tabbedPane.setBorder(null);
+				
+				tabbedPane.addTab("Lập hóa đơn", pBorder);
+				tabbedPane.setBackground(Color.CYAN);
+				getContentPane().add(tabbedPane);
+				tabbedPane.add("Quản lý hóa đơn",pBorder1);
+				getContentPane().add(tabbedPane);
+		
+		
 
 	}
 
